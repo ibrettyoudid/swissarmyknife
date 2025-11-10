2ebe21b3517e69a74fef4428c87a8a009eba331c
Brett Curtis <bcurtis134@gmail.com>
2025-11-10 01:24:47 +0000

    chess: correctly lists moves

--- Chess.hs
+++ Chess.hs
@@ -36,7 +36,7 @@ grid con off = gr
       gr     = crossWith gf [0..7] [0..7]
       gf y x = 
 -}
-data PC = PC { colour :: Colour, piece :: Piece }
+data PC = PC { colour :: Colour, piece :: Piece } deriving (Eq)
 
 instance Show PC where
    show (PC col piece) = show col ++ show piece
@@ -62,19 +62,19 @@ imofl l = IM.fromList $ zip [0..63] $ concat l
 putl l = putStr $ showl l
 putim im = putStr $ showim im
 
-data Colour = White | Black | None deriving (Eq)
+data Colour = White | Black | EmptyC deriving (Eq)
 
 instance Show Colour where
    show Black = "\27[40;37m"
    show White = "\27[47;30m"
-   show None  = "\27[40;37m"
+   show EmptyC  = "\27[40;37m"
 
 startboardl = [map (PC White) [Castle, Knight, Bishop, Queen, King, Bishop, Knight, Castle],
                replicate 8 $ PC White Pawn,
-               replicate 8 $ PC None Empty,
-               replicate 8 $ PC None Empty,
-               replicate 8 $ PC None Empty,
-               replicate 8 $ PC None Empty,
+               replicate 8 $ PC EmptyC Empty,
+               replicate 8 $ PC EmptyC Empty,
+               replicate 8 $ PC EmptyC Empty,
+               replicate 8 $ PC EmptyC Empty,
                replicate 8 $ PC Black Pawn,
                map (PC Black) [Castle, Knight, Bishop, Queen, King, Bishop, Knight, Castle]]
 
@@ -97,7 +97,10 @@ instance Eq Sq where
 
 instance Show Sq where
    show sq = case pos sq of
-      [x, y] -> [chr (97 + x), chr (49 + y)] ++ show None
+      [x, y] -> [chr (97 + x), chr (49 + y)] ++ show EmptyC
+
+instance Ord Sq where
+   compare a b = compare (n a) (n b)
 
 vec x y = [x, y]
 
@@ -122,13 +125,13 @@ createSquare y x = let
       steps = M.fromList $ mapxfx (\v -> takeWhile (/= Off) $ map (at1 masterboard) $ iterate (v <+>) pos1) queenV,
       knight = map singleton $ filter (/= Off) $ map (at1 masterboard . (pos1 <+>)) knightV,
       pawn = map (\(f, t, v) -> (f, t, map (at1 masterboard) $ filter onboard $ map (pos1 <+>) v)) 
-                        [(White, Black, [[-1,  1]]),
-                         (White, Black, [[ 1,  1]]), 
-                         (White, None , [[ 0,  1]] ++ if y == 1 then [[ 0,  2]] else []), 
+                        [(White, Black , [[-1,  1]]),
+                         (White, Black , [[ 1,  1]]), 
+                         (White, EmptyC, [[ 0,  1]] ++ if y == 1 then [[ 0,  2]] else []), 
 
-                         (Black, White, [[-1, -1]]),
-                         (Black, White, [[ 1, -1]]),
-                         (Black, None , [[ 0, -1]] ++ if y == 6 then [[ 0, -2]] else [])]
+                         (Black, White , [[-1, -1]]),
+                         (Black, White , [[ 1, -1]]),
+                         (Black, EmptyC, [[ 0, -1]] ++ if y == 6 then [[ 0, -2]] else [])]
       }
 
 
@@ -138,32 +141,31 @@ pccolour = colour . pc
 
 at board square = SqPC square $ board IM.! n square
 
-data SqPC = SqPC { sq :: Sq, pc :: PC }
+check board sqpc = at board (sq sqpc) == sqpc
+
+data SqPC = SqPC { sq :: Sq, pc :: PC } deriving (Eq)
 
 instance Show SqPC where
    show (SqPC sq pc) = show pc ++ show sq
 
 instance Show Move where
-   show m = show (from m) ++ (if pcpiece (takem m) == Empty
+   show m = show (from m) ++ (if pcpiece (kill m) == Empty
                                  then '-' : show (sq $ to m)
-                                 else 'x' : show (takem m))
+                                 else 'x' : show (kill m))
 
-data Move = Move { from :: SqPC, takem :: SqPC, to :: SqPC, thru :: [Sq] }
+data Move = Move { from :: SqPC, kill :: SqPC, to :: SqPC, thru :: [Sq] }
 
-movessq :: IM.IntMap PC -> Sq -> [[Move]]
-movessq board fromsquare = let
+movessq movesofsqs board fromsquare = let
    from1 = at board fromsquare
    steps1 = steps fromsquare
    stepsl = map snd $ M.toList steps1
    in case pcpiece from1 of
-      Pawn  -> map (\(fromcol, takecol, sqs) -> if pccolour from1 == fromcol 
+      Pawn  -> map (\(fromcol, killcol, sqs) -> if pccolour from1 == fromcol 
                                                    then 
                                                       concatMap promote $
-                                                         movesofsqs board from1 (== takecol) sqs 
+                                                         movesofsqs board from1 (== killcol) sqs 
                                                    else []) 
                                                    $ pawn fromsquare
-            
-
       Empty -> []
       _ -> let
          steps2 = case pcpiece from1 of
@@ -174,19 +176,23 @@ movessq board fromsquare = let
             Knight -> knight fromsquare
          in map (movesofsqs board from1 (/= pccolour from1)) steps2
 
-takeWhile1 pred [] = []
+takeWhile1 pred []     = []
 takeWhile1 pred (x:xs) = if pred x then x : takeWhile1 pred xs else [x]
 
-movesofsqs board from1 takecol tos1 =
-   takeWhile (takecol . pccolour . takem) $ 
-   takeWhile1 ((== Empty) . pcpiece . takem) $ 
-   movesofsqs1 board from1 tos1
+movesofsqs board from1 killcol tos1 =
+   filter (killcol . pccolour . kill) $ 
+   takeWhile1 ((== Empty) . pcpiece . kill) $ 
+   movesofsqs1 board from1 killcol tos1
 
-movesofsqs1 board from1 tos1 = 
-   mapMaybe (\takes2 -> let
-         take1 = last takes2
-         sq1   = sq take1
-         in ifJust (not $ null takes2) $ Move { from = from1, takem = take1, to = SqPC sq1 (pc from1), thru = map sq takes2 }) $ tail $ inits $ map (at board) tos1
+movesofsqs1 board from1 killcol tos1 =
+   filter (killcol . pccolour . kill) $ 
+   movesofsqs2 board from1 killcol tos1
+
+movesofsqs2 board from1 killcol tos1 = 
+   mapMaybe (\kills2 -> let
+         kill1 = last kills2
+         sq1   = sq kill1
+         in ifJust (not $ null kills2) $ Move { from = from1, kill = kill1, to = SqPC sq1 (pc from1), thru = map sq kills2 }) $ tail $ inits $ map (at board) tos1
 
 promote move = let
    t = to move
@@ -199,9 +205,22 @@ promote move = let
             then [SqPC s $ PC Black Queen, SqPC s $ PC Black Knight]
             else [t]
 
-moves board = concatMap (concat . movessq board) masterboardl
+moves1 movesofsqs board = concatMap (concat . movessq movesofsqs board) masterboardl
 
-movesThru board = concatMap (\m -> map (,m) $ thru m) $ moves board
+moves board = moves1 movesofsqs board
+
+movesThru board = multimap $ concatMap (\m -> map (,m) $ thru m) $ moves1 movesofsqs1 board
+
+doMove move board = if (check board $ from move) && (check board $ kill move)
+   then
+      IM.insert (n $ sq $ to   move) (pc $ to move   ) $
+      IM.insert (n $ sq $ from move) (PC EmptyC Empty) board
+   else
+      error $ "This move "++show move++" is invalid on this board"
+
+newMoves board = let
+   moves2 = movesThru board
+   in moves2
 {-
 CnbKQbnC
 pppppppp
@@ -229,5 +248,4 @@ pppppppp
         
 pppppppp
 CnbKQbnC
-
 -}
