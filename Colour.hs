8c9451e74248921e67bfe4e2926ec20bd0d5b6e1
Brett Curtis <bcurtis134@gmail.com>
2026-01-19 09:39:03 +0000

    new module to handle coloured console text

--- Colour.hs
+++ Colour.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LexicalNegation #-}
+
 module Colour where
 
 import Text.Parsec
@@ -5,7 +7,18 @@ import Text.Parsec
 escape = char '\27'
 
 colourStr = do
-    escape
-    char '['
-    i <- sepBy int (char ';')
-    char 'm'
+   escape
+   char '['
+   i <- sepBy int (char ';')
+   char 'm'
+
+type ColourString = [(Char, Char8, Char8)]
+
+instance Show ColourString where
+   show s = showColour s -1 -1
+
+showColour [              ] _      _      = ""
+showColour ((c, fg, bg):xs) lastfg lastbg =
+    (if fg /= lastfg then ('\27[' ++ (show 30 + fg) ++ "m":) else id) $
+    (if bg /= lastbg then ('\27[' ++ (show 40 + bg) ++ "m":) else id)
+    showColour xs fg bg
