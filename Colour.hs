987578e1569a3fee75054144e2b61164a27e039f
Brett Curtis <bcurtis134@gmail.com>
2026-01-20 13:15:29 +0000

    silly errors

--- Colour.hs
+++ Colour.hs
@@ -2,23 +2,24 @@
 
 module Colour where
 
-import Text.Parsec
+import Data.Word
+--import Text.Parsec
 
-escape = char '\27'
-
+--escape = char '\27'
+{-}
 colourStr = do
    escape
    char '['
    i <- sepBy int (char ';')
    char 'm'
-
-type ColourString = [(Char, Char8, Char8)]
+-}
+newtype ColourString = ColourString [(Char, Word8, Word8)]
 
 instance Show ColourString where
-   show s = showColour s -1 -1
-
+   show (ColourString s) = showColour s 255 255 -- dont try and show colour 255, it wont work
+   
 showColour [              ] _      _      = ""
 showColour ((c, fg, bg):xs) lastfg lastbg =
-    (if fg /= lastfg then ('\27[' ++ (show 30 + fg) ++ "m":) else id) $
-    (if bg /= lastbg then ('\27[' ++ (show 40 + bg) ++ "m":) else id)
-    showColour xs fg bg
+    (if fg /= lastfg then (("\27[" ++ show (30 + fg) ++ "m")++) else id) $
+    (if bg /= lastbg then (("\27[" ++ show (40 + bg) ++ "m")++) else id) $
+    c : showColour xs fg bg
